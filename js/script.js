4081f3c548496be5bff35f42e27178901c8a2f78
Emilia <emiliaprzybylek@hotmail.com>
2022-01-23 16:53:52 +0100

    script added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+  const nav = document.querySelector('.navbar-collapse')
+
+  document.addEventListener('click', () => {
+    if (nav.classList.contains('show')) {
+      nav.classList.remove('show')
+    }
+  })
